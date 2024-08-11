48789edba64d1150c50fa9ebcd57ab3a413c465a
AndreBaretta <andrefbaretta@gmail.com>
2024-08-11 16:56:34 -0300

    Defined the Order class

---
+++ BackEnd/src/Order/Order.hpp
@@ -0,0 +1,33 @@
+#ifndef ORDER_2210
+#define ORDER_2210
+
+#include "../Client/Client.hpp"
+#include "../Product/Product.hpp"
+#include <array>
+#include <chrono>
+
+class Order{
+   public:
+      Order(int id, Client* buyer, double price, year_month_day date){}
+      ~Order(){}
+      
+      // Setters
+      bool setId(int);
+      bool setBuyer(Client*);
+      bool setPrice(double);
+      bool setDate(year_month_day);
+
+      // Getters
+      int            getId();
+      Client*        getBuyer();
+      double         getPrice();
+      year_month_day getDate();
+	    
+   private:
+      int            m_id{};
+      Client* 	     m_buyer{};
+      double	     m_price{};
+      year_month_day m_date{};
+};
+
+#endif
