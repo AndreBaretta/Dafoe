7cd2cdc3de4060f677fb794475e7e6fa8278104f
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-10 23:40:17 -0300

    Define Product function members

--- src/Product/Product.hpp
+++ src/Product/Product.hpp
@@ -3,32 +3,35 @@
 
 #include <string>
 #include <string_view>
-
+#include <array>
 class Product{
    public:
-      Product(int, std::string, double);
+      explicit Product(int, std::string, double);
+      explicit Product(int, std::string, double, int);
+      explicit Product(int, std::string, double, int, std::array<char, 13>);
       ~Product();
 
       // Setters
-      bool setId(int);
       bool setName(std::string);
       bool setQuantity(int);
       bool setPrice(double);
-      bool setBarCode(int);
 
       // Getters
       int getId();
       std::string_view getName();
       int getQuantity();
       double getPrice();
+      std::array<char, 13> getBarCode();
       
 
    private:
-      int id{};
-      std::string name{};
-      int quantity{};
-      double price{};
-      char barCode[13];
+      int m_id{};
+      std::string m_name{};
+      int m_quantity{};
+      double m_price{};
+      std::array<char, 13> m_barCode{};
+      bool setId(int);
+      bool setBarCode(std::array<char, 13>);
 
 };
 #endif
