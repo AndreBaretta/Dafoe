822b6369aa66df9316ed22f8b78ef1784d02e259
GMagan <guilherme_magan@hotmail.com>
2024-08-12 16:55:08 -0300

    updated the login screen
    login screen finished for now, API conection with backed needed for the final touches.

--- FrontEnd/my-app/src/components/Login/Login.jsx
+++ FrontEnd/my-app/src/components/Login/Login.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
 import './Login.css'
 
 const Login = () => {
+
+   const [username, setUsername] = useState("");
+   const [password, setPassword] = useState("");
+
+   const handleSubmit = (event) => {
+      event.preventDefault();
+
+      alert("enviando dados " + username + " - " + password);
+   };
+
    return(
       <div className="Login">
-         <form>
+         <form onSubmit={handleSubmit}>
             <div>
-               <input type="user" placeholder="Usuário" />
+               <input type="user" placeholder="Usuário" onChange={(e) => setUsername(e.target.value)} />
                <FaUser className="icon" />
             </div>
             <div>
-               <input type="password" placeholder="Senha" />
+               <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
                <FaLock className="icon" />
             </div>
 
