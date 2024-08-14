a0325aa8e11bd1256b109d57f06739ade81866ca
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-14 00:56:18 -0300

    fix constructors for m_price removal

--- BackEnd/src/Product/Product.cpp
+++ BackEnd/src/Product/Product.cpp
@@ -3,26 +3,22 @@
 #include <string_view>
 #include "Product.hpp"
 
-Product::Product(int id, std::string name, double price)
+Product::Product(int id, std::string name)
 : m_id{id}
 , m_name{name}
-, m_price{price}
 {}
 
-Product::Product(int id, std::string name, double price, int quantity)
+Product::Product(int id, std::string name, int quantity)
 : m_id{id}
 , m_name{name}
-, m_price{price}
 , m_quantity{quantity}
 {}
 
 
-Product::Product(int id, std::string name, double price, int quantity, std::array<char, 13> barCode)
+Product::Product(int id, std::string name, int quantity, std::array<char, 13> barCode)
 : m_id{id}
 , m_name{name}
-, m_price{price}
 , m_quantity{quantity}
-, m_barCode(barCode)
 {}
 
 bool Product::setId(int id){
@@ -35,18 +31,7 @@ bool Product::setName(std::string name){
     return true;
 }
 
-bool Product::setPrice(double price){
-    this->m_price = price;
-    return true;
-}
-
-bool Product::setBarCode(std::array<char, 13> barCode){
-    this->m_barCode = barCode;
-    return true;
-}
 
 int Product::getId(){return this->m_id;}
 std::string_view Product::getName(){return this->m_name;}
 int Product::getQuantity(){return this->m_quantity;}
-double Product::getPrice(){return this->m_price;}
-std::array<char, 13>& Product::getBarCode(){return this->m_barCode;}
