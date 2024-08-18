ae6d64d978540caf032ebca45b71da996ec54797
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-18 02:47:23 -0300

    create, declare and define clientDAO class

---
+++ BackEnd/src/Client/ClientDAO.hpp
@@ -0,0 +1,20 @@
+#ifndef CLIENTDAO_0150
+#define CLIENTDAO_0150
+#include "Client.hpp"
+#include "../DafoeGod/DafoeGod.hpp"
+#include <vector>
+
+class ClientDAO{
+   ClientDAO(DafoeGod& dafoe);
+   public:
+   bool createClient(const std::string& name, const std::string& phoneNumber, const std::string& address, const double bill);
+   bool deleteClient(const int id);
+   std::vector<Client> retrieveClient(const std::string& name);
+   bool updateClient(const int id, const std::string& name, const std::string& phoneNumber, const std::string& address, const double bill);
+   private:
+   static inline int s_id{};
+   DafoeGod& m_theos;   
+};
+
+
+#endif
