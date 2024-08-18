e488a947405229251def9e61391025d9d8bc246c
AndreBaretta <andrefbaretta@gmail.com>
2024-08-18 19:30:16 -0300

    Minor changes

--- BackEnd/src/Product/ProductMNG.hpp
+++ BackEnd/src/Product/ProductMNG.hpp
@@ -7,23 +7,26 @@
 
 class ProductMNG{
    public:
-      ProductMNG(ProductDAO ProductDAO);
+      ProductMNG(ProductDAO& ProductDAO);
       ~ProductMNG();
       // Create
       bool createProduct(const std::string& name, const int manufacturerId, const int quantity,
                          const int category, const std::string& barCode, const std::string& reference,
                          const double cost, const double price);
+
       // Read
       std::vector<Product> queryByName(const std::string& name);
       Product retrieveByID(const int id);
       Product retrieveByReference(const std::string& reference);
       Product retrieveByBarCode(const std::string& barCode);
+
       // Update
       bool updateProduct(const int id, const std::string& name, const int manufacturerId,
                     const int quantity, const int category, const std::string& barCode,
                     const std::string& reference, const double cost, const double price);
       bool updateCategoryPriceByNumber(const int category, const double price);
       bool updateCategoryPriceByPercentage(const int category, const double percentage);
+
       // Delete
       bool deleteProduct(const int id);
 
