68b8e446b81aa9c1b73936efcddd238c6eb40e71
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-22 23:16:57 -0300

    Create categoryDAO class

--- BackEnd/src/Category/CategoryDAO.hpp
+++ BackEnd/src/Category/CategoryDAO.hpp
@@ -1,13 +1,16 @@
 #include "Category.hpp"
 #include <vector>
+#include "../DafoeGod/DafoeGod.hpp"
 
 class CategoryDAO{
 public:
+    CategoryDAO(DafoeGod& dafoe);
     bool createCategory(std::string name);
-    bool deleteCategory();
-    bool updateCategory();
+    bool deleteCategory(const int id);
+    bool updateCategory(const int id, std::string name);
     std::vector<Category> listCategories();
 private:
+    DafoeGod& m_theos;
     static inline int s_id{0};
 
 };
