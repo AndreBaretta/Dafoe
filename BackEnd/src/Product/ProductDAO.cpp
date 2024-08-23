bd660c4233aee124e839e5e8ed94227752ead4fe
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-23 20:34:35 -0300

    fix the queries to use the new database table  genericProduct

--- BackEnd/src/Product/ProductDAO.cpp
+++ BackEnd/src/Product/ProductDAO.cpp
@@ -13,7 +13,7 @@ ProductDAO::ProductDAO(DafoeGod& zeus)
 {}
 
 std::vector<Product> ProductDAO::retrieveByName(const std::string& name){
-    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from product where name like ?")};
+    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from genericProduct where name like ?")};
     state->setString(1, '%' + name + '%');
     sql::ResultSet* result {state->executeQuery()};
 
@@ -26,7 +26,7 @@ std::vector<Product> ProductDAO::retrieveByName(const std::string& name){
 }
 
 Product ProductDAO::retrieveByID(const int id){
-    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from product where id = ?")};
+    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from genericProduct where id = ?")};
     state->setInt(1,id);
     sql::ResultSet* result {state->executeQuery()};
     
@@ -35,7 +35,7 @@ Product ProductDAO::retrieveByID(const int id){
 }
 
 Product ProductDAO::retrieveByBarCode(const std::string& barCode){
-    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from productManufacturer where barcode = ?")};
+    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from product where barcode = ?")};
     state->setString(1,barCode);
     sql::ResultSet* result {state->executeQuery()};
     
@@ -44,7 +44,7 @@ Product ProductDAO::retrieveByBarCode(const std::string& barCode){
 }
 
 Product ProductDAO::retrieveByReference(const std::string& reference){
-    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from productManufacturer where reference like ?")};
+    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("select * from genericProduct where reference like ?")};
     state->setString(1,'%' + reference + '%');
     sql::ResultSet* result {state->executeQuery()};
     
@@ -57,7 +57,7 @@ std::vector<Product> ProductDAO::retrieveByManufacturer(const std::string& manuf
     state->setString(1,'%' + manufacturer + '%');
     sql::ResultSet* result {state->executeQuery()};
 
-    std::unique_ptr<sql::PreparedStatement> statement{m_theos.conn->prepareStatement("select product from productManufacturer where manufacturer =")};
+    std::unique_ptr<sql::PreparedStatement> statement{m_theos.conn->prepareStatement("select genericProduct from product where manufacturer =")};
     statement->setInt(1, result->getInt("id"));
     result = statement->executeQuery();
 
@@ -73,34 +73,35 @@ bool ProductDAO::createProduct(const std::string& name, const int manufacturerId
                                const int category, const std::string& barCode, const std::string& reference,
                                const double cost, const double price){
 
-    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("insert into product (id,name,quantity,category) values (?,?,?,?)")};
-    state->setInt(1, this->s_id);
+    std::unique_ptr<sql::PreparedStatement> state{m_theos.conn->prepareStatement("insert into genericProduct (id,name,quantity,category) values (?,?,?,?)")};
+    state->setInt(1, this->s_id_genericProduct);
     state->setString(2, name);
     state->setInt(3, quantity);
     state->setInt(4, category);
     state->executeQuery();
 
-    std::unique_ptr<sql::PreparedStatement> statement{m_theos.conn->prepareStatement("insert into productManufacturer (product, manufacturer, barcode, price, cost, reference, quantity) values (?, ?, ?, ?, ?, ?, ?)")};
-    statement->setInt(1, this->s_id);
-    statement->setInt(2, manufacturerId);
-    statement->setString(3, barCode);
-    statement->setDouble(4, price);
-    statement->setDouble(5, cost);
-    statement->setString(6, reference);
-    statement->setInt(7, quantity);
+    std::unique_ptr<sql::PreparedStatement> statement{m_theos.conn->prepareStatement("insert into product (id, genericProduct, manufacturer, barcode, price, cost, reference, quantity) values (?, ?, ?, ?, ?, ?, ?)")};
+    statement->setInt(1, this->s_id_product++);
+    statement->setInt(2, this->s_id_genericProduct++);
+    statement->setInt(3, manufacturerId);
+    statement->setString(4, barCode);
+    statement->setDouble(5, price);
+    statement->setDouble(6, cost);
+    statement->setString(7, reference);
+    statement->setInt(8, quantity);
 
     statement->executeQuery();
-    ++s_id;
+
     return true;
 }
 
 bool ProductDAO::deleteProduct(const int id){
-    std::unique_ptr<sql::PreparedStatement> state(m_theos.conn->prepareStatement("delete from product where id = ?"));
+    std::unique_ptr<sql::PreparedStatement> state(m_theos.conn->prepareStatement("delete from genericProduct where id = ?"));
     state->setInt(1, id);
     state->executeQuery();
 
 
-    std::unique_ptr<sql::PreparedStatement> statement(m_theos.conn->prepareStatement("delete from productManufacturer where product = ?"));
+    std::unique_ptr<sql::PreparedStatement> statement(m_theos.conn->prepareStatement("delete from product where genericProduct = ?"));
     statement->setInt(2, id);
     statement->executeQuery();
 
