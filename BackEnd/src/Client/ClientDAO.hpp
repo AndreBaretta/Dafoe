72db2a087e7a51ba857dee8ce3f67feef16dd6c0
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-22 23:24:54 -0300

    fix clientDAO class

--- BackEnd/src/Client/ClientDAO.hpp
+++ BackEnd/src/Client/ClientDAO.hpp
@@ -5,13 +5,17 @@
 #include <vector>
 
 class ClientDAO{
+
+public:
+
    ClientDAO(DafoeGod& dafoe);
-   public:
    bool createClient(const std::string& name, const std::string& phoneNumber, const std::string& address, const double bill);
    bool deleteClient(const int id);
    std::vector<Client> retrieveClient(const std::string& name);
    bool updateClient(const int id, const std::string& name, const std::string& phoneNumber, const std::string& address, const double bill);
-   private:
+
+private:
+
    static inline int s_id{};
    DafoeGod& m_theos;   
 };
