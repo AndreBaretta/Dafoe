fbc65e7226bb0e18d47db5ad584e3166c2841531
GMagan <guilherme_magan@hotmail.com>
2024-08-23 00:46:09 -0300

    fixed a mistake in the css
    there was 2 css classes with the name "icon", now fixed

--- FrontEnd/my-app/src/components/Menu/Menuu.jsx
+++ FrontEnd/my-app/src/components/Menu/Menuu.jsx
@@ -5,32 +5,32 @@ function Menu() {
   <div className="Menu">
    <div className="MenuItem">
        <button onClick={()=>window.open('http://localhost:3000?home','_self')}></button>
-       <div className="icon">☰</div>
+       <div className="Menu-icon">☰</div>
        <span>Menu Principal</span>
      </div>
      <div className="MenuItem">
        <button onClick={()=>window.open('http://localhost:3000?sales','_self')}></button>
-       <div className="icon">📈</div>
+       <div className="Menu-icon">📈</div>
        <span>Vendas</span>
      </div>
      <div className="MenuItem">
        <button onClick={()=>window.open('http://localhost:3000?stock','_self')}></button>
-       <div className="icon">📦</div>
+       <div className="Menu-icon">📦</div>
        <span>Estoque</span>
      </div>
      <div className="MenuItem">
        <button onClick={()=>window.open('http://localhost:3000?history','_self')}></button>
-       <div className="icon">🕒</div>
+       <div className="Menu-icon">🕒</div>
        <span>Registro</span>
      </div>
      <div className="MenuItem">
        <button onClick={()=>window.open('http://localhost:3000?clients','_self')}></button>
-       <div className="icon">👥</div>
+       <div className="Menu-icon">👥</div>
        <span>Clientes</span>
      </div>
      <div className="MenuItem">
        <button onClick={()=>window.open('http://localhost:3000?register','_self')}></button>
-       <div className="icon">+👤</div>
+       <div className="Menu-icon">+👤</div>
        <span>Cadastrar</span>
      </div>
    </div>
