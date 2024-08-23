1269580da4407bd76661f9f4904e1959d45addc2
GMagan <guilherme_magan@hotmail.com>
2024-08-23 00:40:24 -0300

    Updated the menu
    Now all the screens make use of the same menu, making it so that you can go from where you are to wherever you want

--- FrontEnd/my-app/src/components/Home/Home.js
+++ FrontEnd/my-app/src/components/Home/Home.js
@@ -1,33 +1,16 @@
 import './Home.css';
 import Sidebar from '../Sidebar/sidebar';
-import '../Buttons/Menu.css'
+import Menu from '../Menu/Menuu';
 import Daily from '../Daily/Daily';
 
 function Home() {
   return (
     <div className="Home">
       <div className="Home-glass">
-        <header className='Home-header'>
+        <header className='Menu-header'>
           <Sidebar />
-          <div className="content">
-            {/* Menu */}
-            <div className="Menu">
-              <div className="MenuItem">
-                <button onClick={()=>window.open('http://localhost:3000?sales','_self')}></button>
-                <div className="icon">📈</div>
-                <span>Vendas</span>
-              </div>
-              <div className="MenuItem">
-                <button onClick={()=>window.open('http://localhost:3000?stock','_self')}></button>
-                <div className="icon">📦</div>
-                <span>Estoque</span>
-              </div>
-              <div className="MenuItem">
-                <button onClick={()=>window.open('http://localhost:3000?register','_self')}></button>
-                <div className="icon">⚙️</div>
-                <span>Cadastros</span>
-              </div>
-            </div>
+          <div className='content'>
+          <Menu />
           </div>
         </header>
         <Daily />
--- FrontEnd/my-app/src/components/Sales/Sales.js
+++ FrontEnd/my-app/src/components/Sales/Sales.js
@@ -1,19 +1,15 @@
 import './Sales.css';
 import Sidebar from '../Sidebar/sidebar';
-import '../Buttons/BackButton.css'
+import Menu from '../Menu/Menuu';
 
 function Sales() {
   return (
     <div className="Sales">
       <div className="Sales-glass">
-        <div className="backButtonDisplay">
-            <div className="backButton">
-              <button onClick={()=>window.open('http://localhost:3000?home','_self')}></button>
-              <div className="icon">⬅</div>
-              <span>voltar</span>
-            </div>
-          </div>
+        <header className='Menu-header'>
         <Sidebar />
+        <Menu />
+        </header>
       </div>
     </div>
   );
