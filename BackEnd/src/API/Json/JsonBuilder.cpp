3c1d58ee2950b84dd25cd98dfb0c7a383adde107
Andre <andrefbaretta@gmail.com>
2024-08-23 23:16:03 -0300

    Changed the Product Json Builder so that it matches the database product table

--- BackEnd/src/API/Json/JsonBuilder.cpp
+++ BackEnd/src/API/Json/JsonBuilder.cpp
@@ -5,11 +5,13 @@ JsonBuilder::~JsonBuilder(){}
 
 json JsonBuilder::productToJson(Product& product){
    json jsonProduct;
-   jsonProduct["id"]       = product.getId();
-   jsonProduct["name"]     = product.getName();
-   jsonProduct["barcode"]  = product.getBarCode();
-   jsonProduct["quantity"] = product.getQuantity();
-   jsonProduct["category"] = product.getCategory();
+   jsonProduct["id"]        = product.getId();
+   jsonProduct["name"]      = product.getName();
+   jsonProduct["barcode"]   = product.getBarcode();
+   jsonProduct["price"]     = product.getPrice();
+   jsonProduct["cost"]      = product.getCost();
+   jsonProduct["reference"] = product.getReference();
+   jsonProduct["quantity"]  = product.getQuantity();
    return jsonProduct;
 }
 
@@ -17,11 +19,13 @@ json JsonBuilder::productVectorToJson(std::vector<Product>& products){
    json jsonProductArray = json::array();
    for(int i = 0; i < products.size(); i++){
       json jsonProduct;
-      jsonProduct["id"]       = products[i].getId();
-      jsonProduct["name"]     = products[i].getName();
-      jsonProduct["barcode"]  = products[i].getBarCode();
-      jsonProduct["quantity"] = products[i].getQuantity();
-      jsonProduct["category"] = products[i].getCategory();
+      jsonProduct["id"]        = products[i].getId();
+      jsonProduct["name"]      = products[i].getName();
+      jsonProduct["barcode"]   = products[i].getBarcode();
+      jsonProduct["price"]     = products[i].getPrice();
+      jsonProduct["cost"]      = products[i].getCost();
+      jsonProduct["reference"] = products[i].getReference();
+      jsonProduct["quantity"]  = products[i].getQuantity();
       jsonProductArray.push_back(jsonProduct);
    }
    return jsonProductArray;
