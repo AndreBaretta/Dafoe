692561b17023ae487d0ca335e41747952a62f78e
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-23 23:52:08 -0300

    create and define StatusDAO class

--- BackEnd/src/Status/StatusDAO.hpp
+++ BackEnd/src/Status/StatusDAO.hpp
@@ -1,8 +1,15 @@
 #ifndef STATUSDAO_2108
 #define STATUSDAO_2108
-#include ""
+#include "../DafoeGod/DafoeGod.hpp"
+
 class StatusDAO{
-   
+public:
+   StatusDAO(DafoeGod& dafoe);
+   bool createStatus(const std::string& name);
+   bool updateStatus(const int id, const std::string& name);
+private:
+   DafoeGod& m_theos;
+   static inline int s_id{0};
 };
 
 #endif
