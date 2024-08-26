d323786b07c9c3a78fde25e85ac841a91cef8419
Andre <andrefbaretta@gmail.com>
2024-08-26 00:53:13 -0300

    Began implementing the POST method

--- BackEnd/src/API/HTTPRequestHandler/HTTPRequestHandler.cpp
+++ BackEnd/src/API/HTTPRequestHandler/HTTPRequestHandler.cpp
@@ -8,77 +8,125 @@ HTTPRequestHandler::HTTPRequestHandler(ProductMNG& productMNG, HTTPResponseBuild
 HTTPRequestHandler::~HTTPRequestHandler(){}
 
 std::string HTTPRequestHandler::handleRequest(HTTPRequest& request){
-   json responseBody;
-   std::string version;
-   std::string statusCode;
-   std::string statusMessage;
-   std::map<std::string,std::string> headers;
+   json responseBody{};
+   std::string version{"HTTP/1.1"};
+   std::string statusCode{};
+   std::string statusMessage{};
+   std::string stringResponse{};
+   std::map<std::string,std::string> headers{};
 
+   std::string method = request.getMethod();
    std::vector<std::string> path = request.getPath();
    std::string query = request.getQuery();
   
-   // Lidando com o path /api/product
-   if (path.size() == 2 && path[0] == "api" && path[1] == "product"){
-      if (query.empty()) {
-	 responseBody = handleRetrieveAll();
-	 version = "HTTP/1.1";
-	 statusCode = "200";
-	 statusMessage = "OK";
-	 HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
-	 std::string stringResponse = this->m_responseBuilder.buildResponseString(response);
-	 return stringResponse;
+   // Lidando com o metodo GET
+   if(method == "GET"){
+      // Product
+      if (path.size() == 2 && path[0] == "api" && path[1] == "product"){
+	 if (query.empty()) {
+	    responseBody = handleRetrieveAll();
+	    headers["Content-Type"] = "application/json";
+	    statusCode = "200";
+	    statusMessage = "OK";
+	    HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
+	    stringResponse = this->m_responseBuilder.buildResponseString(response);
+	    return stringResponse;
 
-      } else if (query.find("name=") == 0) {
-	 std::string name = query.substr(5);
-	 responseBody = handleQueryProductByName(name);
-	 version = "HTTP/1.1";
-	 statusCode = "200";
-	 statusMessage = "OK";
-	 HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
-	 std::string stringResponse = this->m_responseBuilder.buildResponseString(response);
-	 return stringResponse;
+	 } else if (query.find("name=") == 0) {
+	    std::string name = query.substr(5);
+	    responseBody = handleQueryProductByName(name);
+	    headers["Content-Type"] = "application/json";
+	    statusCode = "200";
+	    statusMessage = "OK";
+	    HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
+	    stringResponse = this->m_responseBuilder.buildResponseString(response);
+	    return stringResponse;
 
-      } else if (query.find("reference=") == 0) {
-	 std::string reference = query.substr(10);
-	 responseBody = handleRetrieveProductByReference(reference);
-	 version = "HTTP/1.1";
-	 statusCode = "200";
-	 statusMessage = "OK";
-	 HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
-	 std::string stringResponse = this->m_responseBuilder.buildResponseString(response);
-	 return stringResponse;
+	 } else if (query.find("reference=") == 0) {
+	    std::string reference = query.substr(10);
+	    responseBody = handleRetrieveProductByReference(reference);
+	    headers["Content-Type"] = "application/json";
+	    statusCode = "200";
+	    statusMessage = "OK";
+	    HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
+	    stringResponse = this->m_responseBuilder.buildResponseString(response);
+	    return stringResponse;
 
-      } else if (query.find("barcode=") == 0) {
-	 std::string barcode = query.substr(8);
-	 responseBody = handleRetrieveProductByBarcode(barcode);
-	 version = "HTTP/1.1";
-	 statusCode = "200";
-	 statusMessage = "OK";
-	 HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
-	 std::string stringResponse = this->m_responseBuilder.buildResponseString(response);
+	 } else if (query.find("barcode=") == 0) {
+	    std::string barcode = query.substr(8);
+	    responseBody = handleRetrieveProductByBarcode(barcode);
+	    headers["Content-Type"] = "application/json";
+	    statusCode = "200";
+	    statusMessage = "OK";
+	    HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
+	    stringResponse = this->m_responseBuilder.buildResponseString(response);
+	    return stringResponse;
+	 }
+      }
+
+      if (path.size() == 3 && path[0] == "api" && path[1] == "product"){
+	 try
+	 {
+	    int temp = std::stoi(path[2]);
+	    responseBody = handleRetrieveProductById(temp);
+	    headers["Content-Type"] = "application/json";
+	    statusCode = "200";
+	    statusMessage = "OK";
+	    HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
+	    stringResponse = this->m_responseBuilder.buildResponseString(response);
+	    return stringResponse;
+	 }
+	 catch(std::invalid_argument const& exception)
+	 {
+	    statusCode = "400";
+	    statusMessage = "Bad Request";
+	    HTTPResponse response = HTTPResponse(version, statusCode, statusMessage, headers, responseBody);
+	    stringResponse = this->m_responseBuilder.buildResponseString(response);
+	    return stringResponse;
+	 }
+      }
+
+      statusCode = "404";
+      statusMessage = "Not Found";
+      HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
+      stringResponse = this->m_responseBuilder.buildResponseString(response);
+      return stringResponse;
+   }
+   if(method == "POST"){
+      if(!headers.contains("Content-Length")){
+	 statusCode = "411";
+	 statusMessage = "Length Required";
+	 HTTPResponse response = HTTPResponse(version, statusCode, statusMessage, headers, responseBody);
+	 stringResponse = this->m_responseBuilder.buildResponseString(response);
 	 return stringResponse;
       }
-   }
-
-   if (path.size() == 3 && path[0] == "api" && path[1] == "product"){
-      int temp = std::stoi(path[2]);
-      responseBody = handleRetrieveProductById(temp);
-      version = "HTTP/1.1";
-      statusCode = "200";
-      statusMessage = "OK";
-      HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
-      std::string stringResponse = this->m_responseBuilder.buildResponseString(response);
+      if(path.size() != 2){
+	 statusCode = "400";
+	 statusMessage = "Bad Request";
+	 HTTPResponse response = HTTPResponse(version, statusCode, statusMessage, headers, responseBody);
+	 stringResponse = this->m_responseBuilder.buildResponseString(response);
+	 return stringResponse;
+      }
+      if(path[0] != "api"){
+	 statusCode = "404";
+	 statusMessage = "Not Found";
+	 HTTPResponse response = HTTPResponse(version, statusCode, statusMessage, headers, responseBody);
+	 stringResponse = this->m_responseBuilder.buildResponseString(response);
+	 return stringResponse;
+      }
+      if(path[1] == "product"){
+      }
+      statusCode = "404";
+      statusMessage = "Not Found";
+      HTTPResponse response = HTTPResponse(version, statusCode, statusMessage, headers, responseBody);
+      stringResponse = this->m_responseBuilder.buildResponseString(response);
       return stringResponse;
    }
 
-
-   responseBody = "Page not found.";
-   version = "HTTP/1.1";
-   statusCode = "404";
-   statusMessage = "Not Found";
-   headers["Content-Type"] = "text/plain";
-   HTTPResponse response = HTTPResponse(version,statusCode,statusMessage,headers,responseBody);
-   std::string stringResponse = this->m_responseBuilder.buildResponseString(response);
+   statusCode = "400";
+   statusMessage = "Bad Request";
+   HTTPResponse response = HTTPResponse(version, statusCode, statusMessage, headers, responseBody);
+   stringResponse = this->m_responseBuilder.buildResponseString(response);
    return stringResponse;
 }
 
