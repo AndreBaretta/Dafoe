c61396730ea0f15f04f3ece51e114e5baa9731fe
GMagan <guilherme_magan@hotmail.com>
2024-08-27 02:51:00 -0300

    now able to search the client too

--- FrontEnd/my-app/src/components/Clients/Clients.js
+++ FrontEnd/my-app/src/components/Clients/Clients.js
@@ -1,17 +1,27 @@
 import './Clients.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Sidebar from '../Sidebar/sidebar';
 import Menu from '../Menu/Menuu';
 import SearchBar from '../SearchBar/SearchBar';
 
 function Clients() {
 
-   const [query, setQuery] = useState ("");
-   const [results, setResults] = useState ("");
- 
-   function getData() {
-     fetch("https://jsonplaceholder.typicode.com/posts").then((Response) => Response.json().then(json=>console.log(json)))
-   }
+  const [searchValue, setSearchValue] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try{
+        const response = await fetch("http://localhost:12354/api/client?name=" + searchValue);
+        const data = await response.json();
+        setResults(data)
+      } catch(error) {
+        console.error(error);
+      }
+    };
+
+    getData();
+  }, [searchValue])
 
   return (
     <div className="Clients">
@@ -21,8 +31,15 @@ function Clients() {
           <Menu />
         </header>
         <header className='SearchBarHeader'>
-          <SearchBar results={results} setResults={setResults}/>
+          <SearchBar results={searchValue} setResults={setSearchValue}/>
         </header>
+        <div>
+          {results.map((item) => (
+            <tr>
+              <td>{item.name}</td>
+            </tr>
+          ))}
+        </div>
       </div>
     </div>
   );
