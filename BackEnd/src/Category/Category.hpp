a7c1f564b230b3c160159ff2d4e9730af5abcab3
Andre <andrefbaretta@gmail.com>
2024-08-28 01:01:31 -0300

    Fixed indentation

--- BackEnd/src/Category/Category.hpp
+++ BackEnd/src/Category/Category.hpp
@@ -4,19 +4,19 @@
 #include <string>
 class Category{
 public:
-    Category(int id, std::string name);
+   Category(int id, std::string name);
 
-    // Setters
-    bool setId(const int id);
-    bool setName(const std::string& name);
-    
-    // Getters
-    std::string getName();
-    int getId();
+   // Setters
+   bool setId(const int id);
+   bool setName(const std::string& name);
+
+   // Getters
+   std::string getName();
+   int getId();
 
 private:
-    std::string m_name{};
-    int m_id{};
+   std::string m_name{};
+   int m_id{};
 
 };
 
