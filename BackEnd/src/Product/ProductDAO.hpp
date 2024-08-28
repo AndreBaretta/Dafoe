7a7338846df21901b8732868a449b84c9c549352
Andre <andrefbaretta@gmail.com>
2024-08-27 21:32:18 -0300

    Fixed all the queries and returns

--- BackEnd/src/Product/ProductDAO.hpp
+++ BackEnd/src/Product/ProductDAO.hpp
@@ -2,6 +2,8 @@
 #define PRODUCTDAO_1836
 #include "../DafoeGod/DafoeGod.hpp"
 #include "Product.hpp"
+#include <mariadb/conncpp.hpp>
+#include <mariadb/conncpp/PreparedStatement.hpp>
 #include <mariadb/conncpp/ResultSet.hpp>
 #include <vector>
 
@@ -9,27 +11,25 @@ class ProductDAO{
 public:
 
    ProductDAO(DafoeGod& zeus);
-   std::vector<Product> retrieveByName(std::string name);
+   std::vector<Product> retrieveByName(const std::string& name);
    Product retrieveByID(const int id);
-   std::vector<Product> retrieveByReference(std::string reference);
-   std::vector<Product> retrieveByManufacturer(std::string manufacturer);
-   Product retrieveByBarCode(std::string barCode);
+   std::vector<Product> retrieveByReference(const std::string& reference);
+   std::vector<Product> retrieveByManufacturer(const std::string& manufacturer);
+   Product retrieveByBarcode(const std::string& barcode);
 
-   bool createProduct(const std::string& name, const int genericProduct, const int manufacturerId,
-                      const std::string& barCode, const double price, const double cost, 
+   bool createProduct(const std::string& name, const int genericProduct, const int manufacturer,
+                      const std::string& barcode, const double price, const double cost, 
                       const std::string& reference, const int quantity);
 
    bool deleteProduct(const int id);
 
-   bool updateProduct(const int id, const std::string& name, const int manufacturerId,
-                      const int quantity, const int category, const std::string& barCode,
-                      const std::string& reference, const double cost, const double price);
+   bool updateProduct(const int id, const std::string& name, const int genericProduct,
+                      const int manufacturer, const std::string& barcode, const double price,
+                      const double cost, const std::string& reference, const int quantity);
 
    bool updateCategoryPriceByNumber(const int category, const double price);
    bool updateCategoryPriceByPercentage(const int category, const double percentage);
 private:
-   static inline int s_id{0};
-   static inline int s_genericProductId{0};
    DafoeGod& m_theos;
 };
 
