02cb9523e2a7ec62a26cd22883a7a035f7440264
Deco <142193005+AndreBaretta@users.noreply.github.com>
2024-08-30 01:15:18 -0300

    Implemented CategoryMNG

---
+++ BackEnd/src/CategoryMNG.hpp
@@ -0,0 +1,24 @@
+#ifndef CATEGORYMNG_0107
+#define CATEGORYMNG_0107
+#include "CategoryDAO.hpp"
+#include "Category.hpp"
+#include "../API/Json/JsonBuilder.hpp"
+#include <vector>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+class CategoryMNG{
+   public:
+      CategoryMNG(CategoryDAO& categoryDAO, JsonBuilder& jsonBuilder);
+      json retrieveAllCategories();
+      json retrieveCategory(const int id);
+      bool createCategory(const std::string& name);
+      bool deleteCategory(const int id);
+      bool updateCategory(const int id, const std::string& name);
+
+   private:
+      CategoryDAO& m_categoryDAO;
+      JsonBuilder& m_jsonBuilder;
+};
+
+#endif
