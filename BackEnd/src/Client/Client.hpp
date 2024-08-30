0793c8653bc0491c1889a90518148820df19f324
MonoHenry <andrepericlesnelli@gmail.com>
2024-08-30 00:40:04 -0300

    Client class setters and getters

--- BackEnd/src/Client/Client.hpp
+++ BackEnd/src/Client/Client.hpp
@@ -1,27 +1,31 @@
 #ifndef CLIENT_2208
 #define CLIENT_2208
+
 #include <string>
 #include <string_view>
-class Client{
-   public:
-      Client(const int id, const std::string& name, const std::string& phoneNumber, const std::string& address, const double bill);      
-      // Setters
-      bool setName(std::string);
-      bool setAddress(std::string);
-      bool setBill(double);
 
-      // Getters
-      int getId();
-      std::string_view getName();
-      std::string_view getAddress();
-      double getBill();
+class Client {
+public:
+    Client(int id, const std::string& name, const std::string& phoneNumber, const std::string& address, double bill);
 
-   private:
-      int m_id{};
-      std::string m_name{};
-      std::string m_address{};
-      double m_bill{};
-      std::string m_phoneNumber{};
+    // Setters
+    bool setName(const std::string& name);
+    bool setAddress(const std::string& address);
+    bool setBill(double bill);
+
+    // Getters
+    int getId() const;
+    std::string_view getName() const;
+    std::string_view getAddress() const;
+    double getBill() const;
+
+private:
+    int m_id{};
+    std::string m_name{};
+    std::string m_address{};
+    double m_bill{};
+    std::string m_phoneNumber{};
 };
 
 #endif
+
