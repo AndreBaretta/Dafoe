b44a56bf88033eb4f07ef1ba89507cf33897f063
luiz <142462703+oluiznascimento@users.noreply.github.com>
2024-09-01 07:23:54 -0300

    Update Login.js

--- FrontEnd/my-app/src/components/Login/Login.js
+++ FrontEnd/my-app/src/components/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import './Login.css';
-import Image from '../../assets/images.jpg'; 
 
 const Login = () => {
    const [username, setUsername] = useState("");
@@ -8,26 +7,36 @@ const Login = () => {
 
    const handleSubmit = (event) => {
       event.preventDefault();
-      alert("enviando dados " + username + " - " + password);
+      alert("Enviando dados: " + username + " - " + password);
+      window.location.href = 'http://localhost:3000?home'; 
    };
 
    return (
       <div className="Login">
          <div className="logo-container">
             <div className="logo">
-               <img src={Image} alt="Logo" />
+               <img src={require('../../assets/images.jpg')} alt="Logo" />
                <span>
                   DA<span>FO</span>E
                </span>
             </div>
          </div>
-
          <form onSubmit={handleSubmit}>
             <div className="input-field">
-               <input type="text" placeholder="Usuário" onChange={(e) => setUsername(e.target.value)} />
+               <input 
+                  type="text" 
+                  placeholder="Usuário" 
+                  value={username} 
+                  onChange={(e) => setUsername(e.target.value)} 
+               />
             </div>
             <div className="input-field">
-               <input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} />
+               <input 
+                  type="password" 
+                  placeholder="Senha" 
+                  value={password} 
+                  onChange={(e) => setPassword(e.target.value)} 
+               />
             </div>
             <div className="recall">
                <label>
@@ -37,7 +46,6 @@ const Login = () => {
             </div>
             <button type="submit">Entrar</button>
          </form>
-
          <div className="watermark">
             ©dafoeformation - 2024
          </div>
