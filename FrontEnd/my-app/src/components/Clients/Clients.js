861cf5c7f1d494ee684d0d41861b02ddc411158a
oluiznascimento <luiz.compscience@gmail.com>
2024-09-02 18:29:59 -0300

    update clients/sales

--- FrontEnd/my-app/src/components/Clients/Clients.js
+++ FrontEnd/my-app/src/components/Clients/Clients.js
@@ -1,75 +1,182 @@
 import './Clients.css';
-import React, { useState, useEffect } from 'react';
 import Sidebar from '../Sidebar/sidebar';
-import Menu from '../Menu/Menuu';
 import SearchBar from '../SearchBar/SearchBar';
-import ReactModal from 'react-modal';
+import Menu from '../Menu/Menuu';
+import React, { useState, useEffect } from 'react';
+import Model from 'react-modal';
+
+Model.setAppElement('#root');
 
 function Clients() {
-   const [searchValue, setSearchValue] = useState("");
+   const [searchValue, setSearchValue] = useState('');
    const [results, setResults] = useState([]);
    const [newClientScreen, setNewClientScreen] = useState(false);
+   const [clientDetails, setClientDetails] = useState({
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+   });
+   const [isPending, setIsPending] = useState(false);
 
    useEffect(() => {
       const getData = async () => {
          try {
-            const response = await fetch("http://localhost:12345/api/client?name=" + searchValue);
+            const response = await fetch(
+               `http://localhost:12345/api/clients?name=${searchValue}`
+            );
             const data = await response.json();
             setResults(data);
          } catch (error) {
-            console.error(error);
+            console.error('Erro ao buscar clientes:', error);
          }
       };
 
       getData();
    }, [searchValue]);
 
+   const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setClientDetails((prevState) => ({
+         ...prevState,
+         [name]: value,
+      }));
+   };
+
+   const submitClientData = async () => {
+      setIsPending(true);
+      try {
+         const response = await fetch('http://localhost:12345/api/clients', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(clientDetails),
+         });
+
+         if (response.ok) {
+            console.log('Cliente adicionado com sucesso');
+            setClientDetails({
+               name: '',
+               email: '',
+               phone: '',
+               address: '',
+            });
+            setNewClientScreen(false);
+            setSearchValue('');
+         } else {
+            console.error('Erro ao adicionar cliente');
+         }
+      } catch (error) {
+         console.error('Erro na requisição:', error);
+      } finally {
+         setIsPending(false);
+      }
+   };
+
    return (
       <div className="Clients">
-         <header className='Menu-header'>
+         <header className="Menu-header">
             <Sidebar />
             <Menu />
          </header>
-         <header className='SearchBarHeader'>
-            <SearchBar results={searchValue} setResults={setSearchValue} />
+         <header className="SearchBarHeader">
             <div className="button-container">
-               <button className="newClientButton" onClick={() => setNewClientScreen(true)}>Novo Cliente</button>
+               <button
+                  className="newClientButton"
+                  onClick={() => setNewClientScreen(true)}
+               >
+                  Novo Cliente
+               </button>
             </div>
+            <SearchBar results={searchValue} setResults={setSearchValue} />
          </header>
          <div className="Clients-table-container">
             <table className="Clients-table">
                <thead>
                   <tr>
-                     <th>Nome</th>
+                     <th>
+                        Nome{' '}
+                        <button onClick={() => console.log('Ordenar por Nome')}></button>
+                     </th>
+                     <th>
+                        Telefone{' '}
+                        <button
+                           onClick={() => console.log('Ordenar por Telefone')}
+                        ></button>
+                     </th>
+                     <th>
+                        Endereço{' '}
+                        <button
+                           onClick={() => console.log('Ordenar por Endereço')}
+                        ></button>
+                     </th>
                   </tr>
                </thead>
                <tbody>
-                  {results.map((item) => (
-                     <tr key={item.id}>
-                        <td>{item.name}</td>
+                  {results.map((client) => (
+                     <tr key={client.id}>
+                        <td>{client.name}</td>
+                        <td>{client.phone}</td>
+                        <td>{client.address}</td>
                      </tr>
                   ))}
                </tbody>
             </table>
          </div>
-         <ReactModal 
-            isOpen={newClientScreen} 
+         <Model
+            isOpen={newClientScreen}
             onRequestClose={() => setNewClientScreen(false)}
             className="ReactModal__Content"
             overlayClassName="ReactModal__Overlay"
+            ariaHideApp={false}
          >
-            <button className="ReactModal__Close" onClick={() => setNewClientScreen(false)}>X</button>
-            <div className="ReactModal__Header">
-               Adicionar Novo Cliente
-            </div>
             <div className="newClient">
-               <label htmlFor="name">Nome:</label>
-               <input id="name" type='text'></input>
-               <label htmlFor="phone">Telefone:</label>
-               <input id="phone" type='text'></input>
-               <button>Salvar</button>
+               <span
+                  className="ReactModal__Close"
+                  onClick={() => setNewClientScreen(false)}
+               >
+                  X
+               </span>
+               <h2>Adicionar Novo Cliente</h2>
+               <form>
+                  <label>
+                     Nome:
+                     <input
+                        type="text"
+                        name="name"
+                        value={clientDetails.name}
+                        onChange={handleInputChange}
+                        placeholder="Digite o nome do cliente"
+                     />
+                  </label>
+                  <label>
+                     Telefone:
+                     <input
+                        type="tel"
+                        name="phone"
+                        value={clientDetails.phone}
+                        onChange={handleInputChange}
+                        placeholder="Digite o telefone do cliente"
+                     />
+                  </label>
+                  <label>
+                     Endereço:
+                     <input
+                        type="text"
+                        name="address"
+                        value={clientDetails.address}
+                        onChange={handleInputChange}
+                        placeholder="Digite o endereço do cliente"
+                     />
+                  </label>
+                  {!isPending && (
+                     <button type="button" onClick={submitClientData}>
+                        Salvar
+                     </button>
+                  )}
+                  {isPending && <button disabled>Salvando...</button>}
+               </form>
             </div>
-         </ReactModal>
+         </Model>
       </div>
    );
 }
