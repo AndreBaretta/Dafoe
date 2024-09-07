d9b9ed78ab3946dc55bbe3be5f46b464c36bf179
luiz <142462703+oluiznascimento@users.noreply.github.com>
2024-09-07 11:14:56 -0300

    Update Home.js

--- FrontEnd/my-app/src/components/Home/Home.js
+++ FrontEnd/my-app/src/components/Home/Home.js
@@ -1,18 +1,15 @@
 import './Home.css';
 import Sidebar from '../Sidebar/sidebar';
 import Menu from '../Menu/Menuu';
-import Daily from '../Daily/Daily';
 
 function Home() {
    return (
-      <div className="Home">
-         <header className='Menu-header'>
-            <Sidebar />
-            <div className='content'>
+      <div className="Register">
+            <header className='Menu-header'>
+               <Sidebar />
                <Menu />
-            </div>
-         </header>
-         <Daily />
+            </header>
+         
       </div>
    );
 }
