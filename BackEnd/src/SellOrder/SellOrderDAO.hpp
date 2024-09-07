315a12d2c785ed19c53ee4fc075885da682af514
MonoHenry <andrepericlesnelli@gmail.com>
2024-09-07 20:16:46 -0300

    create methods for dealing with adding product to sellOrder

--- BackEnd/src/SellOrder/SellOrderDAO.hpp
+++ BackEnd/src/SellOrder/SellOrderDAO.hpp
@@ -9,7 +9,7 @@
 
 class SellOrderDAO{
 public:
-   SellOrderDAO(DafoeGod& zeus);
+   SellOrderDAO(DafoeGod& zeus, ProductDAO& pDAO);
 
    bool createOrder(const int clientId, const int sellerId,
                     const int deliveredBy, const int status, 
@@ -18,6 +18,10 @@ public:
    bool updateOrder(const int id, const int clientId, const int sellerId,
                     const int deliveredBy, const int status, 
                     const int paymentMethod, const std::string& date, const double price);
+
+   bool addProductOrder(const int sellOrder, const int product, const int quantity, const double discount, const double price);
+   bool removeProductOrder(const int sellOrder, const int product);
+   bool updateProductOrder(const int sellOrder, const int product, const int quantity, const double discount, const double price);
 
    std::vector<SellOrder> retrieveOrderById(const int id);
    std::vector<SellOrder> retrieveOrderByClient(const int clientId);
@@ -28,6 +32,7 @@ public:
    
 private:
    DafoeGod& m_theos;
+   ProductDAO& m_pDAO;
 };
 
 #endif
